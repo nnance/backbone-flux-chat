fc115f5d3fc6eaaacbc24699ebf6f564bd945611
Nick Nance <nance.nick@gmail.com>
2015-11-05 07:35:02 -0800

    add header component

---
+++ src/app/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+module.exports = React.createClass({
+  render: function() {
+    return <h1>Flux Chat Client</h1>
+  }
+});
---
+++ test/app/components/header.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import bro from 'jsdom-test-browser';
+import Header from '../../../src/app/components/header';
+
+describe('Header View', function() {
+  let instance;
+
+  before(function (done) { bro.newBrowser(done); });
+
+  beforeEach(function() {
+    instance = TestUtils.renderIntoDocument(<Header/>);
+  });
+
+  it('should render h1 element', function() {
+    const entries = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'h1');
+    expect(entries.length).to.equal(1);
+  });
+});
