3608ced84ed50cf44ace025ba0333fc6f1bdc1d1
Nick Nance <nance.nick@gmail.com>
2015-12-02 23:41:13 -0800

    improve filter component

--- src/components/filter_input.jsx
+++ src/components/filter_input.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 
-export default class FilterInput extends React.Component {
-
+class BasicFilter extends React.Component {
   filterList(event) {
     this.props.filterAction(event.target.value);
   }
 
+  render() {
+    return (
+      <input type="text" placeholder="Search" className="input-sm form-control" onChange={this.filterList.bind(this)} ref="filter"></input>
+    );
+  }
+}
+
+class InputGroup extends BasicFilter {
   triggerAdd() {
     this.props.addAction(this.refs.filter.value);
   }
@@ -17,25 +24,26 @@ export default class FilterInput extends React.Component {
       addButton = <button type="button" className="btn btn-sm btn-primary" onClick={this.triggerAdd.bind(this)}>{this.props.addText}</button>;
     }
 
-    var searchInput;
-    if (this.props.addAction) {
-      searchInput = <div className="input-group">
-          <input type="text" placeholder="Search" className="input-sm form-control" onChange={this.filterList.bind(this)} ref="filter"></input>
-            <span className="input-group-btn">
-              {addButton}
-            </span>
-        </div>;
-    } else {
-      searchInput = <input type="text" placeholder="Search" className="input-sm form-control" onChange={this.filterList.bind(this)} ref="filter"></input>
-    }
+    return (
+      <div className="input-group">
+        <input type="text" placeholder="Search" className="input-sm form-control" onChange={this.filterList.bind(this)} ref="filter"></input>
+          <span className="input-group-btn">
+            {addButton}
+          </span>
+      </div>
+    );
+  }
+}
 
+export default class FilterInput extends React.Component {
+  render() {
     return (
       <div className="row m-b-sm m-t-sm">
         <div className="col-md-1">
           <button type="button" id="loading-example-btn" className="btn btn-white btn-sm" ><i className="fa fa-refresh"></i> Refresh</button>
         </div>
         <div className="col-md-11">
-          {searchInput}
+          {this.props.addAction ? <InputGroup {...this.props}/> : <BasicFilter {...this.props}/>}
         </div>
       </div>
     );
