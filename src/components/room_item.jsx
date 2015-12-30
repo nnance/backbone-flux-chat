72930715f39606e8f852ab176b85f51042f94f41
Nick Nance <nance.nick@gmail.com>
2015-12-30 07:02:58 -0800

    fix room icon spacing

--- src/components/room_item.jsx
+++ src/components/room_item.jsx
@@ -28,8 +28,8 @@ class RoomItem extends React.Component {
           <small>Created 14.08.2014</small>
         </td>
         <td className="project-actions">
-          <a href="#" className="btn btn-white btn-sm" onClick={this.view.bind(this)}><i className="fa fa-folder"></i>View</a>
-          <a href="#" className="btn btn-white btn-sm" onClick={this.selected.bind(this)}><i className="fa fa-pencil"></i>Join</a>
+          <a href="#" className="btn btn-white btn-sm" onClick={this.view.bind(this)}><i className="fa fa-folder"></i> View</a>
+          <a href="#" className="btn btn-white btn-sm" onClick={this.selected.bind(this)}><i className="fa fa-pencil"></i> Join</a>
         </td>
       </tr>
     );
